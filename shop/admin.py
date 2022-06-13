aa1996168ce9d6b3cc194112a947105faf0b105f
david <davidkost26@gmail.com>
2022-06-13 13:41:38 +0300

    init and feat: -Product catalog, shopping cart(using sessions), custom context processors. -Celery with RabbitMQ as a massage broker, and monitoring with Flower. -Integrated a payment system with Braintree. -Exporting orders to CSV files and PDF(WeasyPrint) invoce feat. -Custom views for the administration site.

---
+++ shop/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # The value is automatically set using the value of other fields.
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    # The value is automatically set using the value of other fields.
+    prepopulated_fields = {'slug': ('name',)}
+
